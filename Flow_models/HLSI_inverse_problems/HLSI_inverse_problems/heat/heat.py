9350034847ef9ca084b8f1cccfbee05804d6df8b
alduston <59751919+alduston@users.noreply.github.com>
2026-04-16 16:45:19 -0500

    Update heat.py

--- Flow_models/HLSI_inverse_problems/HLSI_inverse_problems/heat/heat.py
+++ Flow_models/HLSI_inverse_problems/HLSI_inverse_problems/heat/heat.py
@@ -1964,8 +1964,8 @@ SAMPLER_CONFIGS = OrderedDict([
         {
             'init': 'prior',
             'init_steps': 0,
-            'mala_steps': 32,
-            'mala_burnin': 12,
+            'mala_steps': 500,
+            'mala_burnin': 100,
             'mala_dt': 1e-4,
             'is_reference': True,
         },
