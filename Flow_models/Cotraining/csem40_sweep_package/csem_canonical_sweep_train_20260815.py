4d73a28ae7ad6849e80a872e6b75e989907b1940
alduston <59751919+alduston@users.noreply.github.com>
2026-08-24 13:38:01 -0700

    Delete Flow_models/Cotraining/csem40_sweep_package directory

--- Flow_models/Cotraining/csem40_sweep_package/csem_canonical_sweep_train_20260815.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Unique entry point for the 2026-08-15 canonical CSEM screen."""
-
-from csem_canonical_stable_20260813 import main
-
-
-if __name__ == "__main__":
-    main()
